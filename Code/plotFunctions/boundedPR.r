8b871e0eae9a396024c471a7df1973ecca09150d
Ben Howes <b.howes19@imperial.ac.uk>
2023-08-25 10:30:55 +0100

    Created new analysis code for the parallel runs of the MTaNa

--- Code/plotFunctions/boundedPR.r
+++ Code/plotFunctions/boundedPR.r
@@ -1,6 +1,8 @@
 ##############################################
-## Create new formula for Poff
-## Where Poff = 0 at H < 0 and 1 at H > 1
+## Create new formula for pR
+## Where pR = pZ (probability of death)
+## when H = 0
+## So the growth rate is 0 at H = 0
 ##############################################
 
 library(tidyverse)
@@ -8,21 +10,24 @@ library(tidyverse)
 gpath = "/home/ben/Documents/TangledMT/Paper/Figures/"
 setwd(gpath)
 
-x = seq(-10, 10, length.out = 101)
+x = seq(-100, 1000, length.out = 1001)
+pZ = 0.2
 masses = seq(log10(0.01), log10(100), length.out = 3)
 masses = 10^masses
-dat = expand.grid(x, masses) %>% rename("x" = 1, "m" = 2)
+dat = expand.grid(x, masses) %>% 
+    rename("x" = 1, "m" = 2)
 
-pOff = function(x, m, k = 0.1) {
+pR = function(x, m, k = 0.1, pZ) {
+    x0 = log((1/pZ)-1)/k
     G = 5*m^0.25
-    pOff = 1 / (1 + (exp(-(x - 0.5))))
-    pOff = pOff/G
-    return(pOff)
+    pR = 1 / (1 + (exp(-k*(x - x0))))
+    pR = pR/G
+    return(pR)
 }
 
-dat = dat %>% mutate(G = m^0.25, pOff = pOff(x, m, k = 1))
+dat = dat %>% mutate(G = m^0.25, pR = pR(x, m, k = 0.01, pZ = 0.2))
 
-pOffPlot = ggplot(dat, aes(x, pOff, col = as.factor(round(log10(m), 2)))) + 
+pRPlot = ggplot(dat, aes(x, pR, col = as.factor(round(log10(m), 2)))) + 
     geom_line(linewidth = 2.5) +
     theme_classic() +
     labs(x = expression(paste(H[i], "'")), y = expression(paste(p[R],sep="")),
@@ -30,6 +35,6 @@ pOffPlot = ggplot(dat, aes(x, pOff, col = as.factor(round(log10(m), 2)))) +
     theme(text = element_text(size = 30)) +
     scale_colour_viridis_d()
 
-pOffPlot
+pRPlot
 
-ggsave(filename = "pOffPlot.pdf", plot = pOffPlot,  width = 18, height = 10)
+ggsave(filename = "pRPlot.pdf", plot = pRPlot,  width = 18, height = 10)
